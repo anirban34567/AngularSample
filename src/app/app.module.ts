b112036d532e8525b13dd37c31bf8d6d4ecb8aee
anirban34567 <117729041+anirban34567@users.noreply.github.com>
2023-06-16 00:41:38 +0530

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, RoutesRecognized } from '@angular/router';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { EmployeeViewComponent } from './employee/employee-view.component';
+import { EmployeeCreateComponent } from './employee/employee-create.component';
+import { Routes } from '@angular/router';
+
+
+const myroutes:Routes = [
+  { path: 'view', component: EmployeeViewComponent },
+  { path: 'create', component: EmployeeCreateComponent }
+]
+@NgModule({
+  declarations: [
+    AppComponent,
+    EmployeeViewComponent,
+    EmployeeCreateComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule.forRoot(myroutes)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/employee/employee-create.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { Employee } from '../Model/Employee';
+
+@Component({
+  selector: 'app-employee-create',
+  templateUrl: './employee-create.component.html',
+  styleUrls: ['./employee-create.component.css']
+})
+export class EmployeeCreateComponent 
+{
+  employee:Employee[] = [
+    {
+      firstName : "Rohan",
+      lastName : "Mishra",
+      dateOfBirth : new Date()
+    },
+    {
+      firstName : "Sumit",
+      lastName : "Das",
+      dateOfBirth : new Date()
+    },
+    {
+      firstName : "Sanjay",
+      lastName : "Sinha",
+      dateOfBirth : new Date()
+    },
+    {
+      firstName : "Rajiv",
+      lastName : "Kumar",
+      dateOfBirth : new Date()
+    },
+    {
+      firstName : "Soham",
+      lastName : "Das",
+      dateOfBirth : new Date()
+    }
+  ];
+
+}
